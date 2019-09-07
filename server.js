88b00a4aaf1b59259731af18ab8b8466c3790dc3
arghp <arpangurung@hotmail.com>
2019-09-07 00:05:44 -0500

    Fixed typo

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json({ extended: false }));
 app.get('/', (req,res) => res.send('API Running'));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/user'));
+app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/profiles', require('./routes/api/profiles'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/messages', require('./routes/api/messages'));
 
